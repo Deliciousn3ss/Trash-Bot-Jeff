000d970b1c06db30f15df45cbce940d0f7f8761a
vaughn127 <43101324+vaughn127@users.noreply.github.com>
2023-04-24 15:41:53 -0500

    added object tracking, updated object scanning, added a process note

--- Computer_Vision_Stuff/Object_Tracking.py
+++ Computer_Vision_Stuff/Object_Tracking.py
@@ -12,8 +12,8 @@ from time import sleep
 from math import radians, pi
 from array import *   
 
-def objectScanner(colortarget):    #Blue = 0, Orange = 1, Green = 2
 
+def objectTracking(colortarget):
     HSV =   [[[55,45,125],[255,255,255]],           
             [[0,90,195],[255,255,255]],
             [[45,50,110],[255,255,255]]]
@@ -35,18 +35,6 @@ def objectScanner(colortarget):    #Blue = 0, Orange = 1, Green = 2
     angle_margin = 0.2      # Radians object can be from image center to be considered "centered"
     width_margin = 10       # Minimum width error to drive forward/back
 
-    try:
-        camera = cv2.VideoCapture(0)    
-    except: pass
-
-    # Try opening camera stream if default method failed
-    if not camera.isOpened():
-        camera = cv2.VideoCapture(camera_input)    
-
-    camera.set(3, size_w)                       # Set width of images that will be retrived from camera
-    camera.set(4, size_h)                       # Set height of images that will be retrived from camera
-
-    objectFound = 0
     while (objectFound < 1):
 
         ret, image = camera.read()  # Get image from camera
@@ -66,13 +54,19 @@ def objectScanner(colortarget):    #Blue = 0, Orange = 1, Green = 2
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)      # Close morph: fills openings w/ dilate followed by erode
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
                 cv2.CHAIN_APPROX_SIMPLE)[-2]                        # Find closed shapes in image
-        
-        if len(cnts) and len(cnts) < 3:                             # If more than 0 and less than 3 closed shapes exist
-            print(cnts,"objects found!")
-            objectFound = cnts #Breaks loop when at least one object is found
-            return objectFound
 
-        
+    try:
+        camera = cv2.VideoCapture(0)    
+    except: pass
+
+    # Try opening camera stream if default method failed
+    if not camera.isOpened():
+        camera = cv2.VideoCapture(camera_input)    
+
+    camera.set(3, size_w)                       # Set width of images that will be retrived from camera
+    camera.set(4, size_h)                       # Set height of images that will be retrived from camera
+    
+    return
 
 def getIp():
     for interface in ni.interfaces()[1:]:   #For interfaces eth0 and wlan0
@@ -85,35 +79,3 @@ def getIp():
             continue                        #Try the next interface since this one has no IPv4
         
     return 0
-
-
-if __name__ == '__main__':
-    objectScanner(colortarget=0)                       
-
-#blue ball
-#v1_min = 55      # Minimum H value
-#v2_min = 45     # Minimum S value
-#v3_min = 125      # Minimum V value
-
-#v1_max = 255     # Maximum H value
-#v2_max = 255    # Maximum S value
-#v3_max = 255    # Maximum V value
-
-
-#orange ball
-#v1_min = 0      # Minimum H value
-#v2_min = 90     # Minimum S value
-#v3_min = 195      # Minimum V value
-
-#v1_max = 255     # Maximum H value
-#v2_max = 255    # Maximum S value
-#v3_max = 255    # Maximum V value
-
-#green ball
-#v1_min = 45      # Minimum H value
-#v2_min = 50     # Minimum S value
-#v3_min = 110      # Minimum V value
-
-#v1_max = 255     # Maximum H value
-#v2_max = 255    # Maximum S value
-#v3_max = 255    # Maximum V value
