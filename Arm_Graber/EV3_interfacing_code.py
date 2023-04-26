e56dd2fc0c7f74bf74795d499486011148674e09
“Deliciousn3ss” <reconraider7@gmail.com>
2023-04-26 12:28:47 -0500

    Trying to fix the bluetooth rfcomm0

--- Arm_Graber/EV3_interfacing_code.py
+++ Arm_Graber/EV3_interfacing_code.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 #====================================#
 #   LEGO Mindstorms EV3 Interfacer   #
 #====================================#
@@ -10,13 +11,21 @@ sudo apt-get install bluetooth bluez libbluetooth-dev
 sudo python3 -m pip install pybluez
 '''
 
-import bluetooth
+# if bluetooth is not working on the pi:
+# systemctl start hciuart
+
 import serial
 import time
 import EV3BT
 
+EV3 = serial.Serial('/dev/rfcomm0')
+s = EV3BT.encodeMessage(EV3BT.MessageType.Text, 'abc', 'Eat responsibly')
+print(EV3BT.printMessage(s))
+EV3.write(s)
+time.sleep(1)
+EV3.close()
 
-
+'''
 # Front claw interfacing
 
 #set EV3 to bluetooth bus comm0
@@ -25,6 +34,7 @@ try:
 
 except OSError:
     print("EV3 Brick not found! Exiting...")
+    exit()
 
 
 
@@ -32,7 +42,7 @@ except OSError:
 print("Waiting for message")
 
 try:
-    s = EV3BT.encodeMessage(EV3BT.MessageType.Numeric, "abc", "1")
+    s = EV3BT.encodeMessage(EV3BT.MessageType.Numeric, "Motor state", "1")
     print(EV3BT.printMessage(s))
     EV3.write(s)
     time.sleep(1)
@@ -43,3 +53,4 @@ except KeyboardInterrupt:
     pass
 
 EV3.close()
+'''
